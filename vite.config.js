b102be1bbd18ca60f2b1e6d9883045213a1e1e78
naaaa2 <113960780+naaaa2@users.noreply.github.com>
2022-10-19 16:24:21 +0900

    first

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        // outDir に manifest.json を出力
+        manifest: true,
+        rollupOptions: {
+           // デフォルトの .html エントリを上書き
+          input: '/path/to/main.js'
+        }
+    },
     server: {
         hmr: {
             host: 'us-cdbr-east-06.cleardb.net',
