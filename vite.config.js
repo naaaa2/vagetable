b12c154447082f2ebbcf6e101568b68ff125f522
naaaa2 <113960780+naaaa2@users.noreply.github.com>
2022-10-19 16:27:05 +0900

    first

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import 'vite/modulepreload-polyfill';
 
 export default defineConfig({
-    build: {
-        // outDir に manifest.json を出力
-        manifest: true,
-        rollupOptions: {
-           // デフォルトの .html エントリを上書き
-          input: '/path/to/main.js'
-        }
-    },
     server: {
         hmr: {
             host: 'us-cdbr-east-06.cleardb.net',
