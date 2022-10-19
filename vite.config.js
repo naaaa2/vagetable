77d634b131c5a0dedb5a51e472dee3943f184ea6
naaaa2 <113960780+naaaa2@users.noreply.github.com>
2022-10-19 17:19:23 +0900

    first

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,13 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    server: {
+        host: true
+      },
 });
