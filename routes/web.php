67429de06e5d5edd26771af4368b9595ac95e56a
naaaa2 <113960780+naaaa2@users.noreply.github.com>
2022-10-19 16:01:58 +0900

    first

--- routes/web.php
+++ routes/web.php
@@ -65,9 +65,9 @@ Route::get('/oauth/{provider}/callback', CallbackFromProviderController::class)-
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
