e8d49790edfebb18e0c6584d0e6b0402a6ee5c4d
naaaa2 <113960780+naaaa2@users.noreply.github.com>
2022-11-08 15:07:32 +0900

    first

--- resources/views/shopping/list.blade.php
+++ resources/views/shopping/list.blade.php
@@ -9,7 +9,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <!-- <div class="p-6 bg-white border-b border-gray-200"> -->
+                <div class="p-6 bg-white border-b border-gray-200">
                 <form class="w-full md:max-w-md flex gap-2" method="post" action= "{{ route('shopping.store') }}">
                 @csrf
                 <input type="text" name="mustbuy" placeholder="入力してください" class="w-full flex-1 bg-gray-50 text-gray-800 placeholder-gray-500 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2" />
